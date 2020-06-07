f21f79e94dc5b2b9d90f7ad21b2eb8fe06087262
Muhammad Arslan <muhammada@buildinary.com>
2020-06-07 09:14:14 +0500

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
+import { SidebarModule } from './sidebar/sidebar.module';
+// import { FooterModule } from './shared/footer/footer.module';
+// import { NavbarModule} from './shared/navbar/navbar.module';
+// import { FixedPluginModule} from './shared/fixedplugin/fixedplugin.module';
+
 import { AppComponent } from './app.component';
+import { AppRoutes } from './app.routing';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(AppRoutes, { useHash: true }),
+        SidebarModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'dashboard-cmp',
+    templateUrl: 'dashboard.component.html'
+})
+
+export class DashboardComponent {
+
+}
